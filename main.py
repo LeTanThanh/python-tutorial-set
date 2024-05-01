dbfa53f6d0d6faa0a8072c426744a3f6d2a574a4
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 14:00:38 +0700

    Introduction to the Python Set type

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 if __name__ == "__main__":
-  print("Python Set")
+  # Introduction to the Python Set type
+
+  skills = {  "Python programming", "Databases", "Software design" }
+  print(skills)
+
+  empty_set = set()
+  print(empty_set)
+
+  if not empty_set:
+    print("Empty sets are falsy")
+
+  skills = set(["Problem solving", "Critical Thinking"])
+  print(skills)
+
+  characters = set("letter")
+  print(characters)
